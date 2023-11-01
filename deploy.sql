8ed8d7542180acfbfc8c2238008b17f40ddb9fcb
viveksundar5 <134808541+viveksundar5@users.noreply.github.com>
2023-11-01 22:50:41 +0530

    Update deploy.sql

--- deploy.sql
+++ deploy.sql
@@ -2,7 +2,21 @@
 USE master;
 GO
 
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'YourDatabaseName')
 BEGIN
-    CREATE DATABASE Vive;
+    CREATE DATABASE YourDatabaseName;
 END;
+GO
+
+-- Use the newly created database
+USE YourDatabaseName;
+GO
+
+-- Create a sample table
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100)
+);
 GO
